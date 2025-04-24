fb5ba554f453efb4d5b37bbd4f9c38ded158ac0e
Naydenov <nikola.naydenov@limechain.tech>
2025-04-24 10:20:55 +0300

    chore: spotlessApply
    Signed-off-by: Naydenov <nikola.naydenov@limechain.tech>

--- sdk/src/testIntegration/java/com/hedera/hashgraph/sdk/test/integration/ClientIntegrationTest.java
+++ sdk/src/testIntegration/java/com/hedera/hashgraph/sdk/test/integration/ClientIntegrationTest.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
