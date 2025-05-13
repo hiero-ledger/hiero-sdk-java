b03f5a2351715e0fc0c289bea6509501ba89fd3e
Naydenov <nikola.naydenov@limechain.tech>
2025-05-13 14:19:34 +0300

    release-2.56.0
    Signed-off-by: Naydenov <nikola.naydenov@limechain.tech>

--- example-android/app/build.gradle.kts
+++ example-android/app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation(platform("org.hiero:hiero-dependency-versions"))
     // ---------------------------------------------
 
-    implementation("com.hedera.hashgraph:sdk:2.55.0")
+    implementation("com.hedera.hashgraph:sdk:2.56.0")
 
     implementation("com.google.android.material:material:1.11.0")
 
