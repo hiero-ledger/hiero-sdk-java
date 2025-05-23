30721b1a135cc916679e0e809f10740531a1ac06
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-05-23 13:19:42 +0200

    build: bump Gradle to 8.14 / gradle-conventions to 0.4.1 / example deps (#2343)
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- example-android/app/build.gradle.kts
+++ example-android/app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 
     implementation("com.hedera.hashgraph:sdk:2.56.1")
 
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation("androidx.appcompat:appcompat")
     implementation("androidx.constraintlayout:constraintlayout")
@@ -67,5 +67,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 
     runtimeOnly("io.grpc:grpc-okhttp")
-    runtimeOnly("org.slf4j:slf4j-simple:2.0.12")
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.17")
 }
