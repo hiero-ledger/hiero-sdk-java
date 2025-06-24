44b818044916a39fe4bb67651a62fc92e9236e63
Nikola Naydenov <nikola.naydenov@limechain.tech>
2025-06-24 11:11:33 +0300

    JSON-RPC method endpoint for TokenCancelAirdropTransaction (#2385)
    Signed-off-by: Naydenov <nikola.naydenov@limechain.tech>

---
+++ tck/src/main/java/com/hedera/hashgraph/tck/methods/sdk/param/token/TokenAirdropCancelParams.java
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+package com.hedera.hashgraph.tck.methods.sdk.param.token;
+
+import com.hedera.hashgraph.tck.methods.JSONRPC2Param;
+import com.hedera.hashgraph.tck.methods.sdk.param.CommonTransactionParams;
+import com.hedera.hashgraph.tck.util.JSONRPCParamParser;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenAirdropCancelParams extends JSONRPC2Param {
+    private Optional<List<PendingAirdropParams>> pendingAirdrops;
+    private Optional<CommonTransactionParams> commonTransactionParams;
+
+    @Override
+    public JSONRPC2Param parse(Map<String, Object> jrpcParams) throws Exception {
+        @SuppressWarnings("unchecked")
+        var parsedPendingAirdrops = Optional.ofNullable((List<Object>) jrpcParams.get("pendingAirdrops"))
+                .map(list -> list.stream()
+                        .map(item -> {
+                            try {
+                                @SuppressWarnings("unchecked")
+                                Map<String, Object> airdropMap = (Map<String, Object>) item;
+                                return PendingAirdropParams.parse(airdropMap);
+                            } catch (Exception e) {
+                                throw new RuntimeException("Failed to parse pending airdrop parameters", e);
+                            }
+                        })
+                        .collect(Collectors.toList()));
+
+        var parsedCommonTransactionParams = JSONRPCParamParser.parseCommonTransactionParams(jrpcParams);
+
+        return new TokenAirdropCancelParams(parsedPendingAirdrops, parsedCommonTransactionParams);
+    }
+}
