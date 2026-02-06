83624050470ac0a1e26b41daf713e3e98892ed14
Manish Dait <daitmanish88@gmail.com>
2026-02-06 22:48:39 +0530

    chore: apply code style formating
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- tck/src/main/java/com/hedera/hashgraph/tck/methods/sdk/response/FileInfoResponse.java
+++ tck/src/main/java/com/hedera/hashgraph/tck/methods/sdk/response/FileInfoResponse.java
@@ -1,14 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
 package com.hedera.hashgraph.tck.methods.sdk.response;
 
-import lombok.Data;
-
 import java.util.List;
+import lombok.Data;
 
 @Data
 public class FileInfoResponse {
     private final String fileId;
     private final String size;
-    private final  String expirationTime;
+    private final String expirationTime;
     private final boolean isDeleted;
     private final String fileMemo;
     private final String ledgerId;
