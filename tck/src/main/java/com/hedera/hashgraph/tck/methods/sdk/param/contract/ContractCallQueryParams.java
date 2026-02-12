ac514b6922d8525795f26af44388c2bed94acf8a
Mustafa Uzun <mustafa.uzun@limechain.tech>
2026-02-12 10:48:29 +0200

    style: formatting
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- tck/src/main/java/com/hedera/hashgraph/tck/methods/sdk/param/contract/ContractCallQueryParams.java
+++ tck/src/main/java/com/hedera/hashgraph/tck/methods/sdk/param/contract/ContractCallQueryParams.java
@@ -4,7 +4,6 @@ package com.hedera.hashgraph.tck.methods.sdk.param.contract;
 import com.hedera.hashgraph.tck.methods.JSONRPC2Param;
 import com.hedera.hashgraph.tck.util.JSONRPCParamParser;
 import java.util.Map;
-import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
