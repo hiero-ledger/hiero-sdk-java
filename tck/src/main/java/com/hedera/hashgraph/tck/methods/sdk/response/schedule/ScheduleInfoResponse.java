76df946441e94312016a2e2b13feda0c8c8220b1
Manish Dait <daitmanish88@gmail.com>
2026-02-17 21:06:16 +0530

    chore:override function to ingnore null values
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- tck/src/main/java/com/hedera/hashgraph/tck/methods/sdk/response/schedule/ScheduleInfoResponse.java
+++ tck/src/main/java/com/hedera/hashgraph/tck/methods/sdk/response/schedule/ScheduleInfoResponse.java
@@ -5,10 +5,12 @@ import jakarta.annotation.Nullable;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import net.minidev.json.JSONAware;
+import net.minidev.json.JSONObject;
 
 @Data
 @AllArgsConstructor
-public class ScheduleInfoResponse {
+public class ScheduleInfoResponse implements JSONAware {
     private String scheduleId;
     private String creatorAccountId;
     private String payerAccountId;
@@ -38,5 +40,29 @@ public class ScheduleInfoResponse {
 
     public static ScheduleInfoResponse forCostOnly(String cost) {
         return new ScheduleInfoResponse(null, null, null, null, null, null, null, null, null, null, null, cost);
+    }
+
+    @Override
+    public String toJSONString() {
+        JSONObject json = new JSONObject();
+        putIfNotNull(json, "scheduleId", scheduleId);
+        putIfNotNull(json, "creatorAccountId", creatorAccountId);
+        putIfNotNull(json, "payerAccountId", payerAccountId);
+        putIfNotNull(json, "adminKey", adminKey);
+        putIfNotNull(json, "signers", signers);
+        putIfNotNull(json, "scheduleMemo", scheduleMemo);
+        putIfNotNull(json, "expirationTime", expirationTime);
+        putIfNotNull(json, "executed", executed);
+        putIfNotNull(json, "deleted", deleted);
+        putIfNotNull(json, "scheduledTransactionId", scheduledTransactionId);
+        putIfNotNull(json, "waitForExpiry", waitForExpiry);
+        putIfNotNull(json, "cost", cost);
+        return json.toJSONString();
+    }
+
+    private void putIfNotNull(JSONObject json, String key, Object value) {
+        if (value != null) {
+            json.put(key, value);
+        }
     }
 }
