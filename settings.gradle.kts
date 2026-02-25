2bc61b0fce56734fdf70bbb6f1f268fa83ea765c
Jendrik Johannes <jendrik.johannes@gmail.com>
2026-02-25 14:25:22 +0100

    build: fix dependencies issues caused by json-snapshot / update Gradle (#2638)
    Signed-off-by: Jendrik Johannes <jendrik@onepiece.software>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
-plugins { id("org.hiero.gradle.build") version "0.6.0" }
+import org.gradlex.jvm.dependency.conflict.resolution.JvmDependencyConflictsExtension
+
+plugins { id("org.hiero.gradle.build") version "0.7.4" }
 
 rootProject.name = "hedera-sdk-java"
 
@@ -12,3 +14,19 @@ javaModules {
 includeBuild("examples")
 
 includeBuild("example-android")
+
+gradle.lifecycle.beforeProject {
+    plugins.withId("org.gradlex.jvm-dependency-conflict-resolution") {
+        the<JvmDependencyConflictsExtension>().patch {
+            module("io.github.json-snapshot:json-snapshot") {
+                // clean up unused dependencies from
+                // https://github.com/json-snapshot/json-snapshot.github.io/blob/master/pom.xml
+                removeDependency("org.junit.jupiter:junit-jupiter-engine")
+                removeDependency("org.junit.platform:junit-platform-runner")
+                removeDependency("org.junit.vintage:junit-vintage-engine")
+                removeDependency("org.mockito:mockito-junit-jupiter")
+                addRuntimeOnlyDependency("junit:junit")
+            }
+        }
+    }
+}
