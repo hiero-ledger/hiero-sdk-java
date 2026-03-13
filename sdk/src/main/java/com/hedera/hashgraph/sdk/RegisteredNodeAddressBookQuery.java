81571518090e362a46db6b07084781c2e09f6d4d
Manish Dait <daitmanish88@gmail.com>
2026-03-13 18:19:59 +0530

    feat: added class structure for registeredNodeAdressBook query
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

---
+++ sdk/src/main/java/com/hedera/hashgraph/sdk/RegisteredNodeAddressBookQuery.java
@@ -0,0 +1,29 @@
+package com.hedera.hashgraph.sdk;
+
+import java.util.concurrent.CompletableFuture;
+
+public class RegisteredNodeAddressBookQuery {
+    // TODO: Implementation should be deferred until the mirror node API is available
+    private String nodeType;
+
+    public RegisteredNodeAddressBookQuery setNodeType(String nodeType) {
+        this.nodeType = nodeType;
+        return this;
+    }
+
+    public String getNodeType() {
+        return this.nodeType;
+    }
+
+    public RegisteredNodeAddressBook execute(Client client) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    private CompletableFuture<String> executeMirrorNodeRequest(Client client) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    private RegisteredNodeAddressBook parseRegisterNodeAddressBook(String responseBody) {
+        throw new RuntimeException("Method not implemented");
+    }
+}
