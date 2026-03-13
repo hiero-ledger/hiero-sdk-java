f31bae750d9c1fe680859a5e694d9464c3211ea6
Manish Dait <daitmanish88@gmail.com>
2026-03-13 18:44:04 +0530

    feat: added e2e test for the registeredNodeTx
    Signed-off-by: Manish Dait <daitmanish88@gmail.com>

--- sdk/src/testIntegration/java/com/hedera/hashgraph/sdk/test/integration/RegisteredNodeCreateTransactionIntegrationTest.java
+++ sdk/src/testIntegration/java/com/hedera/hashgraph/sdk/test/integration/RegisteredNodeCreateTransactionIntegrationTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.hedera.hashgraph.sdk.BlockNodeApi;
 import com.hedera.hashgraph.sdk.BlockNodeServiceEndpoint;
+import com.hedera.hashgraph.sdk.MirrorNodeServiceEndpoint;
 import com.hedera.hashgraph.sdk.PrivateKey;
 import com.hedera.hashgraph.sdk.RegisteredNodeCreateTransaction;
 import com.hedera.hashgraph.sdk.RegisteredServiceEndpoint;
@@ -23,6 +24,34 @@ public class RegisteredNodeCreateTransactionIntegrationTest {
                     .setDomainName("test.block.com")
                     .setPort(443)
                     .setEndpointApi(BlockNodeApi.STATUS));
+
+            var response = new RegisteredNodeCreateTransaction()
+                    .setAdminKey(key)
+                    .setDescription("test description")
+                    .setServiceEndpoints(serviceEndpoints)
+                    .freezeWith(testEnv.client)
+                    .execute(testEnv.client);
+
+            var receipt = response.getReceipt(testEnv.client);
+
+            assertThat(receipt.status).isEqualTo(Status.SUCCESS);
+            assertThat(receipt.registeredNodeId).isGreaterThan(0);
+        }
+    }
+
+    @Test
+    @DisplayName("Can create a registered node with multiple service endpoints")
+    void canCreateRegisteredNodeWithMultipleServiceEndpoints() throws Exception {
+        try (var testEnv = new IntegrationTestEnv(1)) {
+            var key = PrivateKey.generateED25519();
+            List<RegisteredServiceEndpoint> serviceEndpoints = List.of(
+                    new BlockNodeServiceEndpoint()
+                            .setDomainName("test.block.com")
+                            .setPort(443)
+                            .setEndpointApi(BlockNodeApi.STATUS),
+                    new MirrorNodeServiceEndpoint()
+                            .setDomainName("test.mirror.com")
+                            .setPort(443));
 
             var response = new RegisteredNodeCreateTransaction()
                     .setAdminKey(key)
