f6359fe8d3e9074acf394b09c179abd8b41b5ef8
Mustafa Uzun <mustafa.uzun@limechain.tech>
2026-04-27 11:07:13 +0300

    chore: update spring-boot version
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hiero-dependency-versions/build.gradle.kts
+++ hiero-dependency-versions/build.gradle.kts
@@ -8,7 +8,7 @@ val slf4j = "2.0.17"
 val mockito = "5.23.0"
 
 dependencies {
-    api(platform("org.springframework.boot:spring-boot-dependencies:4.0.5"))
+    api(platform("org.springframework.boot:spring-boot-dependencies:4.0.6"))
     api(platform("io.netty:netty-bom:4.2.12.Final"))
 }
 
